043b027bf5e46d437b760b91d6bea672690fed07
Omar <omar.lf.lahia@gmail.com>
2025-01-26 17:08:19 -0500

    Fourth Commit

--- my-react-app/src/Investigating.jsx
+++ my-react-app/src/Investigating.jsx
@@ -1,13 +1,12 @@
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import './Investigating.scss';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import AverageRemainingTime from './math/AverageRemainingTime.jsx';
+import "./Investigating.scss";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Divider from "@mui/material/Divider";
+import ScienceIcon from "@mui/icons-material/Science";
+import ImageIcon from "@mui/icons-material/Image";
 
 
 function Investigating({ userId }) {
-
   const [patient, setPatient] = useState(null); // Assuming fetching a single patient
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -16,7 +15,9 @@ function Investigating({ userId }) {
     const fetchPatient = async () => {
       try {
         // Removed colon before userId
-        const response = await axios.get(`http://localhost:5000/api/patients/${userId}`);
+        const response = await axios.get(
+          `http://localhost:5000/api/patients/${userId}`
+        );
         setPatient(response.data);
         setLoading(false);
       } catch (err) {
@@ -30,43 +31,44 @@ function Investigating({ userId }) {
       fetchPatient();
     } else {
       setLoading(false);
-      setError(new Error('No user ID provided'));
+      setError(new Error("No user ID provided"));
     }
   }, [userId]); // Added userId as a dependency
 
   if (loading) return <p>Loading patient data...</p>;
   if (error) return <p>Error fetching patient data: {error.message}</p>;
 
-
-
-    return (
-        <div className='Investigating'>
-        {patient ? (
-            <div className="sectionWaiting">
-            <h1 className="titre">{patient.status_current_phase}</h1>
-            <div className="waitingQueue">
-                <p>Your position in the queue is: </p>
-                <h3 className="pos">{patient.queue_position_global}</h3>
+  return (
+    <div className="Investigating">
+      {patient ? (
+        <div className="section">
+          <h1 className="titre">{patient.status_current_phase}</h1>
+          <Divider variant="middle" style={{ width: "60vw", margin: 0 }} />
+          <div className="queue">
+            <h2>We are Investigating potential issues</h2>
+            <div className="annexes">
+              <div className="annex-stat">
+                <p>Laboratory Status</p>
+                <ScienceIcon style={{ color: "green", fontSize: "3rem" }} />
+                <p>{patient.labs_status}</p>
+              </div>
+              <div className="annex-stat">
+                <p>Imaging Status</p>
+                <ImageIcon style={{ color: "green", fontSize: "3rem" }} />
+                <p>{patient.imaging_status}</p>
+              </div>
             </div>
-            <div className="onglet">
-                <ButtonGroup variant="text" size="large" aria-label="Medium-sized button group">
-                    <Button>FAQ</Button>
-                </ButtonGroup>
-            </div>
-            <div className="lastUpdate">
-                <p>Your information was updated 1 minute ago.</p>
-            </div>
-            <div className="timeLeft">
-                <AverageRemainingTime triage_category={patient.triage_category} elapsed_time={patient.time_elapsed} />
-            </div>
+          </div>
+          <Divider variant="middle" style={{ width: "60vw", margin: '2rem' }} />
+          <div className="lastUpdate">
+            <p>Your information was updated 1 minute ago.</p>
+          </div>
         </div>
-        ) : (<div>User not found</div>)}
-            
-        </div>
-
-    );
-
-
-};
+      ) : (
+        <div>User not found</div>
+      )}
+    </div>
+  );
+}
 
 export default Investigating;
